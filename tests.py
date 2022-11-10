e252951a26d8a5322a9573ba3ab51b02c40fb236
Matthew Wong <wongmat@oregonstate.edu>
2022-11-09 17:24:37 -0800

    allen HAHAHAAAAA

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import task
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        expected = "Hello World"
+        expected = "Hello World!"
         self.assertEqual(task.my_func(), expected)
 
     def test2(self):
